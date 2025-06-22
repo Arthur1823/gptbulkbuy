3acf83c65360e2c826c0d1bf0e722796d5125d48
Arthur1823 <ufukkara1823@gmail.com>
2025-06-22 13:33:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Announcement from './components/Announcement';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
   return (
     <>
+      <Announcement />
       <Header />
       <Hero />
     </>
