ec999f1f7db0b2f5d13debbb2543de38dc11378d
Arthur1823 <ufukkara1823@gmail.com>
2025-06-22 13:21:06 +0300

    Create Announcement.jsx

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Announcement from './components/Announcement';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+      <Announcement />
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
